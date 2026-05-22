0d3d062a21e95351aa2823e2f4327c15ae759677
Anonymous <andarass18@gmail.com>
2026-05-22 23:54:13 +0700

    add and update portfolio project. fix some text and layouting.

--- detail-project.js
+++ detail-project.js
@@ -19,10 +19,10 @@ const projects = {
     description:
       "This page contains the case study of Thriftshop Wear which includes the Project Overview, Tools Used and Live Demo to the official product.",
     overview: [
-        "Thriftshop Wear is a simple template UI of e-commerce website built for selling secondhand clothing online. The platform comes with essential features such as a product catalog, about, and contact, making the buying process smooth and user-friendly.",
-        "The project was designed with a clean and minimal User Interface to ensure easy navigation for customers. By focusing on simplicity and functionality, Thriftshop Wear aims to provide an accessible online shopping experience for those looking for affordable and sustainable fashion choices.",
-        "Feel free to check out the project by visiting the Live Demo.",
-      ],
+      "Thriftshop Wear is a simple template UI of e-commerce website built for selling secondhand clothing online. The platform comes with essential features such as a product catalog, about, and contact, making the buying process smooth and user-friendly.",
+      "The project was designed with a clean and minimal User Interface to ensure easy navigation for customers. By focusing on simplicity and functionality, Thriftshop Wear aims to provide an accessible online shopping experience for those looking for affordable and sustainable fashion choices.",
+      "Feel free to check out the project by visiting the Live Demo.",
+    ],
     image: "./assets/portfolio/project-2-thriftshop.png",
     tech: ["html5-plain", "css3-plain", "javascript-plain"],
     github: "https://github.com/andarass/website-thriftshop.git",
@@ -33,10 +33,10 @@ const projects = {
     description:
       "This page contains the case study of Psychocare Wear which includes the Project Overview, Tools Used and Live Demo to the official product.",
     overview: [
-        "Psychocare is a simple template UI of a mental health website built for providing online consultations and sharing the latest news about mental well-being. The platform comes with essential features such as a consultation booking system, headline news, about, and contact pages, making the user experience smooth and supportive.",
-        "The project was designed with a clean and minimal User Interface to ensure easy navigation for users seeking help. By focusing on simplicity and accessibility, Psychocare aims to provide a welcoming online environment for those looking for mental health support and information.",
-        "Feel free to check out the project by visiting the Live Demo."
-      ],
+      "Psychocare is a simple template UI of a mental health website built for providing online consultations and sharing the latest news about mental well-being. The platform comes with essential features such as a consultation booking system, headline news, about, and contact pages, making the user experience smooth and supportive.",
+      "The project was designed with a clean and minimal User Interface to ensure easy navigation for users seeking help. By focusing on simplicity and accessibility, Psychocare aims to provide a welcoming online environment for those looking for mental health support and information.",
+      "Feel free to check out the project by visiting the Live Demo.",
+    ],
     image: "./assets/portfolio/project-3-psychocare.png",
     tech: ["html5-plain", "css3-plain", "javascript-plain"],
     github: "https://github.com/andarass/psychocare.git",
@@ -47,12 +47,18 @@ const projects = {
     description:
       "This page contains the case study of KitaPTN which includes the Project Overview, Tools Used and Live Demo to the official product.",
     overview: [
-        "KitaPTN is an educational platform designed to help students prepare for admission into Indonesian public universities (PTN). The website provides learning materials and practice tests aligned with the latest exam standards, ensuring that students can study effectively and measure their readiness.",
-        "With a focus on accessibility and ease of use, KitaPTN offers a structured way for learners to review subjects, take mock exams, and track their progress. The platform aims to support students in achieving their academic goals by combining updated content with a user-friendly interface.",
-        "Feel free to check out the project by visiting the Live Demo.",
-      ],
+      "KitaPTN is an educational platform designed to help students prepare for admission into Indonesian public universities (PTN). The website provides learning materials and practice tests aligned with the latest exam standards, ensuring that students can study effectively and measure their readiness.",
+      "With a focus on accessibility and ease of use, KitaPTN offers a structured way for learners to review subjects, take mock exams, and track their progress. The platform aims to support students in achieving their academic goals by combining updated content with a user-friendly interface.",
+      "Feel free to check out the project by visiting the Live Demo.",
+    ],
     image: "./assets/portfolio/project-4-kitaptn.png",
-    tech: ["tailwindcss-original", "bootstrap-plain", "laravel-plain", "php-plain", "mysql-plain"],
+    tech: [
+      "tailwindcss-original",
+      "bootstrap-plain",
+      "laravel-plain",
+      "php-plain",
+      "mysql-plain",
+    ],
     github: "https://github.com/andarass/UTBK.git",
     demo: "https://github.com/andarass/UTBK.git",
   },
@@ -61,14 +67,98 @@ const projects = {
     description:
       "This page contains the case study of Perpustakaan Fransiskus Lawang which includes the Project Overview, Tools Used and Live Demo to the official product.",
     overview: [
-        "Perpustakaan Fransiskus Lawang is a modern web-based library management system built to improve efficiency in handling book data and student borrowing activities. The website integrates several key features, including QR code scanning for instant book detail access, Dompdf for generating digital borrowing receipts, and Excel import/export to simplify data management for users and students.",
-        "This project was developed with a focus on practicality and automation, helping librarians minimize manual work while providing students with an easier way to browse and borrow books. By combining clean design with robust functionality, Perpustakaan Fransiskus Lawang supports a smoother and more organized digital library experience.",
-        "Feel free to check out the project by visiting the Live Demo.",
-      ],
+      "Perpustakaan Fransiskus Lawang is a modern web-based library management system built to improve efficiency in handling book data and student borrowing activities. The website integrates several key features, including QR code scanning for instant book detail access, Dompdf for generating digital borrowing receipts, and Excel import/export to simplify data management for users and students.",
+      "This project was developed with a focus on practicality and automation, helping librarians minimize manual work while providing students with an easier way to browse and borrow books. By combining clean design with robust functionality, Perpustakaan Fransiskus Lawang supports a smoother and more organized digital library experience.",
+      "Feel free to check out the project by visiting the Live Demo.",
+    ],
     image: "./assets/portfolio/project-5-perpustakaan.png",
-    tech: ["laravel-plain", "php-plain", "mysql-plain", "tailwindcss-original", "alpinejs-original", "docker-plain", "excel-original", "dompdf-plain",],
+    tech: [
+      "laravel-plain",
+      "php-plain",
+      "mysql-plain",
+      "tailwindcss-original",
+      "alpinejs-original",
+      "docker-plain",
+      "excel-original",
+      "dompdf-plain",
+    ],
     github: "https://github.com/Duta46/perpustakaan-barcode-system.git",
     demo: "https://perpustakaansekolahfransiskuslawang.my.id/",
+  },
+  parkir: {
+    title: "Parkir",
+    previewText:
+      "Smart parking management system with barcode scanning and real-time monitoring",
+    description:
+      "This page contains the case study of Perpustakaan Fransiskus Lawang which includes the Project Overview, Tools Used and Live Demo to the official product.",
+    overview: [
+      "Parkir Universitas PGRI Kanjuruhan Malang is a modern web-based parking management system built to improve efficiency in handling campus vehicle parking activities. The website integrates several key features, including barcode scanning for parking entry and exit, automatic parking area and slot detection, vehicle plate number tracking, parking entry time recording, and automatic parking fee calculation with PDF ticket generation for daily parking usage.",
+      "The system also provides an integrated parking exit scanning feature where each completed parking transaction is automatically recorded and calculated into the admin statistics dashboard in real time. Management access is restricted only to parking staff and administrators, allowing them to manage users, add parking areas, and organize parking categories based on user status such as lecturers, students, or staff members.",
+      "This project was developed with a focus on automation, monitoring, and operational efficiency to help parking officers manage campus parking activities more effectively while providing users with a faster and more organized parking experience.",
+      "Feel free to check out the project by visiting the Live Demo.",
+    ],
+    image: "./assets/portfolio/6.png",
+    tech: [
+      "laravel-plain",
+      "php-plain",
+      "mysql-plain",
+      "tailwindcss-original",
+      "alpinejs-original",
+      "docker-plain",
+      "excel-original",
+      "dompdf-plain",
+    ],
+    github: "https://github.com/Duta46/parkir-app.git",
+    demo: "https://github.com/Duta46/parkir-app.git",
+  },
+  sporton: {
+    title: "SportOn",
+    previewText: "Modern e-commerce platform for sports equipment shopping",
+    description:
+      "This page contains the case study of SportOn which includes the Project Overview, Tools Used and Live Demo to the official product.",
+    overview: [
+      "SportOn is a modern e-commerce website built for buying sports equipment and apparel online. The platform provides a seamless shopping experience through features such as a responsive pop-up shopping cart, checkout page, payment page, and real-time payment status tracking, allowing users to complete transactions easily and efficiently.",
+      "The project also includes a dedicated admin dashboard that can only be accessed by administrators. This management system is equipped with product management, category management, bank information settings, and transaction management, all fully integrated in real time to ensure smooth monitoring and operational efficiency.",
+      "This project was developed with a focus on responsive design, usability, and real-time integration to create a modern and reliable e-commerce experience for both customers and administrators.",
+      "Feel free to check out the project by visiting the Live Demo.",
+    ],
+    image: "./assets/portfolio/7.png",
+    tech: [
+      "react-original",
+      "tailwindcss-original",
+      "typescript-plain",
+      "express-original",
+      "mongodb-plain",
+      "figma-plain",
+    ],
+    github: "https://github.com/andarass/sports-on-live-session.git",
+    demo: "https://sports-on-live-session.vercel.app/",
+  },
+  farmlace: {
+    title: "Farmlace",
+    previewText:
+      "Hybrid farm management application with real-time monitoring and reporting system",
+    description:
+      "This page contains the case study of Perpustakaan Fransiskus Lawang which includes the Project Overview, Tools Used and Live Demo to the official product.",
+    overview: [
+      "Farmlace is a hybrid farm management application built to help livestock companies monitor and manage farm operations more efficiently through both web and mobile platforms. The system integrates several key features, including QR code scanning for employee attendance tracking, attendance history management, and real-time monitoring of farm activities on a daily, weekly, and monthly basis.",
+      "The platform also provides a cage management system containing a list of client companies along with their operational status such as open or closed farms. Inside each company section, users can access detailed farm statistics supported by interactive chart visualizations generated from employee report histories over time. These reports include important information such as livestock quantity, average weight, sapronak usage, operational issues, and other farm condition reports submitted directly by field employees.",
+      "This project was developed with a focus on monitoring, data visualization, and operational efficiency to help farm management teams oversee livestock development more effectively while simplifying reporting activities for employees in the field. By combining responsive design with integrated real-time reporting, Farmlace delivers a modern and organized digital farm management experience.",
+      "Feel free to check out the project by visiting the Live Demo.",
+    ],
+
+    image: "./assets/portfolio/8.png",
+    tech: [
+      "vuejs-plain",
+      "typescript-plain",
+      "ionic-original",
+      "adonisjs-original",
+      "postman-plain",
+      "postgresql-plain",
+      "figma-plain",
+    ],
+    github: "https://github.com/josephvoxone/ionic-internship.git",
+    demo: "https://github.com/josephvoxone/ionic-internship.git",
   },
 };
 
@@ -136,3 +226,12 @@ if (projectId && projects[projectId]) {
 } else {
   window.location.href = "index.html#projects";
 }
+
+// text preview
+Object.entries(projects).forEach(([id, project]) => {
+  const previewEl = document.querySelector(`#preview-${id}`);
+
+  if (previewEl) {
+    previewEl.textContent = project.previewText;
+  }
+});
